c051069d2f9e25587478802280fd0bed789bd65d
MariiaO28 <idg.masha@gmail.com>
2024-05-12 20:09:36 +0300

    fixed css movie list and movie card

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -6,8 +6,8 @@ export default function MovieCard({ movie: { title,poster_path } }) {
         <div className={css.container}>
             {poster_path === null ?
                 <img src={`https://cdn-icons-png.freepik.com/512/2270/2270642.png`} alt={title} width='300px'className={css.picture}/>
-                : <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={title} width='200px' className={css.picture}/>}
-            <h3 className={css.title}>{ title }</h3>
+                : <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={title} width='200px' className={css.picture} />}
+                <h3 className={css.title}>{title}</h3>
         </div>
     )
 }
